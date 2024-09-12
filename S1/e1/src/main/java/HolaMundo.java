7c274967c1d8babe4a1427b3202523288b9184a3
Andrés Ramírez <andres200493@gmail.com>
2024-09-11 19:50:09 -0600

    S1 -> E1

---
+++ S1/e1/src/main/java/HolaMundo.java
@@ -0,0 +1,13 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡¡¡Hola Mundo!!!");
+    }
+
+    public String saluda(){
+        return "¡¡¡Hola Mundo!!!";
+    }
+
+    public int numero(){
+        return 500;
+    }
+}
