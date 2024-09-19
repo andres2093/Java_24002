0c3f7917550b0b2ec9595619dd3da91b161cf3a0
Andrés Ramírez <andres200493@gmail.com>
2024-09-18 19:43:15 -0600

    S2 -> R1

---
+++ s2/src/main/java/r1/R1.java
@@ -0,0 +1,35 @@
+package r1;
+
+public class R1 {
+    public static void main(String[] args) {
+        byte maxByte = Byte.MAX_VALUE;
+        System.out.println("maxByte " + maxByte);
+        maxByte++;
+        System.out.println("maxByte " + maxByte);
+
+        byte minByte = Byte.MIN_VALUE;
+        System.out.println("minByte " + minByte);
+        minByte++;
+        System.out.println("minByte " + minByte);
+
+        int maxInt = Integer.MAX_VALUE;
+        System.out.println("maxInt " + maxInt);
+        maxInt++;
+        System.out.println("maxInt " + maxInt);
+
+        long maxLong = Long.MAX_VALUE;
+        System.out.println("maxLong " + maxLong);
+        maxLong++;
+        System.out.println("maxLong " + maxLong);
+
+        float maxFloat = Float.MAX_VALUE;
+        System.out.println("maxFloat " + maxFloat);
+        maxFloat++;
+        System.out.println("maxFloat " + maxFloat);
+
+        double maxDouble = Double.MAX_VALUE;
+        System.out.println("maxDouble " + maxDouble);
+        maxDouble++;
+        System.out.println("maxDouble " + maxDouble);
+    }
+}
