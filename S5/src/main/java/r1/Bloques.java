73f37e23271d968fefdd9c156b81a7b3910919f3
Andrés Ramírez <andres200493@gmail.com>
2024-09-30 20:05:52 -0600

    S5 -> R1

---
+++ S5/src/main/java/r1/Bloques.java
@@ -0,0 +1,13 @@
+package r1;
+
+public class Bloques {
+    private static final int VALOR = 99;
+
+//    static {
+//        VALOR = 2;
+//    }
+
+    public static void muestraValor(){
+        System.out.println("Valor: " + VALOR);
+    }
+}
